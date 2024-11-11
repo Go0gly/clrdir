bb75a87461fa993030ffa09a075cdf88f0b2fcfa
googly <c.p.hamilton@proton.me>
2024-11-11 18:35:35 -0500

    Added better error messages

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ path = None
 rmdir = False
 
 # Processing arguments
+if len(args) == 0:
+    print(f"\033[31;1mError\033[m: No directory provided")
+    sys.exit(1)
+
 for index,arg in enumerate(args):
     if index == 0 and arg not in keywords:
         path = arg
@@ -23,14 +27,16 @@ for index,arg in enumerate(args):
         for _ in args[index+1::]:
             if _ not in keywords:
                 ignore_list.append(_)
-            else:
-                break
     elif arg == "-rm" or arg == "--rmdir":
         rmdir = True
 
 def clrdir(path: str, ignore_list: list, rmdir: bool):
     original_path = os.getcwd()
-    os.chdir(path)
+    try:
+        os.chdir(path)
+    except:
+        print(f"\033[31;1mError\033[m: Unable to change directory to {path}")
+        sys.exit(1)
     for file in os.listdir():
         if file in ignore_list:
             continue
@@ -39,6 +45,9 @@ def clrdir(path: str, ignore_list: list, rmdir: bool):
                 os.remove(file)
             except IsADirectoryError:
                 clrdir(file, ignore_list, True)
+                print(f"Entering {file}")
+            else:
+                print(f"Deleting {file}")
     os.chdir(original_path)
     if rmdir:
         try:
